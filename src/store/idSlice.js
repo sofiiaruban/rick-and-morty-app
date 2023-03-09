3317dfa9b5c9931104f60b8256f30c219d17a7ad
Sofia Ruban <sophiaruban249324@gmail.com>
2023-03-09 18:21:46 +0200

    feat: Add id slice

---
+++ src/store/idSlice.js
@@ -0,0 +1,14 @@
+const initialState = {
+  value: 0,
+};
+export const idSlice = createSlice({
+  name: id,
+  initialState,
+  reducers: {
+    updateId: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+export const { updateId } = idSlice.actions;
+export default idSlice.reducer;
