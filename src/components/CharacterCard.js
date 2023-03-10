2acb0b68dad24e8d4ad6d8835d0f8b783217d4d5
Sofia Ruban <sophiaruban249324@gmail.com>
2023-03-10 19:39:06 +0200

    styles: Remove unnecessary styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,9 +4,11 @@ import style from "./CharacterCard.module.scss";
 export const CharacterCard = ({ src, name, species }) => {
   return (
     <div className={style.card}>
-      <Img src={src} name={name} classes={style.img} />
-      <h3>{name}</h3>
-      <p>{species}</p>
+      <Img src={src} name={name} classes={`${style.img} `} />
+      <div className={style.description}>
+        <h3 className={style.h3}>{name}</h3>
+        <p>{species}</p>
+      </div>
     </div>
   );
 };
